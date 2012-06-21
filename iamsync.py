e73c2e75379504dcd9c948fb1b5fbbce9a0dea46
James Maritn <james.s.martin@gmail.com>
2012-06-21 11:53:41 -0400

    Fixed group copying.  Updated README.
    Group copying was horridly broken.  Major clean up.

--- iamsync.py
+++ iamsync.py
@@ -8,7 +8,7 @@ import urllib2
 
 def get_group_users(users_response):
     user_list = []
-    for user in users['get_group_response']['get_group_result']['users']:                
+    for user in users_response:
         user_list.append(user['user_name'])    
     return user_list
 
@@ -21,14 +21,15 @@ def build_search_words(args):
     return search_words   
 
 
-def add_users(conn, args, users, group):
-    for user_name in users:
-        if not dest_users_dict.has_key(user_name):
+def add_users(conn, users_dict, args, src_users, dest_users, group):
+    for user_name in src_users:
+        if not users_dict.has_key(user_name):
             print 'Creating user: %s' % user_name
             conn.create_user(user_name, '/')
+            users_dict[user_name] = 0
         else:
-            print 'User: %s already exists' % user_name   
-        if group and users.count(user_name) == 0:
+            print 'User: %s already exists' % user_name
+        if group and dest_users.count(user_name) == 0:
             print 'Adding user %s to group %s' % (user_name, group_name)
             conn.add_user_to_group(group_name, user_name)
         else:
@@ -38,7 +39,8 @@ def add_users(conn, args, users, group):
             for policy_name in policies['list_user_policies_response']['list_user_policies_result']['policy_names']:
                 policy = iam_src.get_user_policy(user_name, policy_name)
                 policy_json = urllib2.unquote(policy['get_user_policy_response']['get_user_policy_result']['policy_document'])
-                iam_dst.put_user_policy(user_name, policy_name, policy_json)  
+                iam_dest.put_user_policy(user_name, policy_name, policy_json)  
+    return users_dict
 
     
 
@@ -64,9 +66,7 @@ def get_options():
             dest='users', nargs='+', help='space separated list of users')
     parser.add_argument('--user-policies', action='store_true',  
             dest='user_policies', default=False, help='copy policies with user')     
-          
-
-   
+             
     args = parser.parse_args()
     return args
 
@@ -80,12 +80,12 @@ if __name__=="__main__":
     
     iam_src = boto.iam.connection.IAMConnection(aws_access_key_id=config.get('src', 'aws_access_key_id'), 
                                                 aws_secret_access_key= config.get('src', 'aws_secret_access_key'))
-    iam_dst = boto.iam.connection.IAMConnection(aws_access_key_id=config.get('dest', 'aws_access_key_id'), 
+    iam_dest = boto.iam.connection.IAMConnection(aws_access_key_id=config.get('dest', 'aws_access_key_id'), 
                                                 aws_secret_access_key= config.get('dest', 'aws_secret_access_key'))
     
     
-    dest_groups = iam_dst.get_all_groups('/')
-    dest_users = iam_dst.get_all_users('/')
+    dest_groups = iam_dest.get_all_groups('/')
+    dest_users = iam_dest.get_all_users('/')
     dest_groups_dict = {}
     dest_users_dict = {}
     
@@ -98,32 +98,38 @@ if __name__=="__main__":
     
         for group_name in args.groups:
             try:
-                users = iam_src.get_group(group_name)
+                src_group_users = iam_src.get_group(group_name)
             except Exception, err:
                 print Exception, err                
-        if not dest_groups_dict.has_key(group_name):
-            iam_dst.create_group(group_name, '/')
-        else:
-            print 'Group: %s already exists.' % group_name
-        if args.group_policies:
-            policies = iam_src.get_all_group_policies(group_name)
-            for policy_name in policies['list_group_policies_response']['list_group_policies_result']['policy_names']:
-                policy = iam_src.get_group_policy(group_name, policy_name)
-                policy_json = urllib2.unquote(policy['get_group_policy_response']['get_group_policy_result']['policy_document'])
-                if args.substitute:
-                    search_words = build_search_words(args)
-                    for find_string, replace_string in search_words.items():
-                        policy_json = policy_json.replace(find_string, replace_string)                
-                iam_dst.put_group_policy(group_name, policy_name, policy_json)        
-                            
-        if args.recursive:
-            group_user_list = get_group_users (users['get_group_response']['get_group_result']['users'] )           
-            add_users(iam_dst, args, group_user_list, group)
-        
+            if not dest_groups_dict.has_key(group_name):
+                print 'Creating group: %s' % group_name
+                iam_dest.create_group(group_name, '/')
+            else:
+                print 'Group: %s already exists.' % group_name
+            if args.group_policies:
+                policies = iam_src.get_all_group_policies(group_name)
+                for policy_name in policies['list_group_policies_response']['list_group_policies_result']['policy_names']:
+                    policy = iam_src.get_group_policy(group_name, policy_name)
+                    policy_json = urllib2.unquote(policy['get_group_policy_response']['get_group_policy_result']['policy_document'])
+                    if args.substitute:
+                        search_words = build_search_words(args)
+                        for find_string, replace_string in search_words.items():
+                            policy_json = policy_json.replace(find_string, replace_string)                
+                    iam_dest.put_group_policy(group_name, policy_name, policy_json)        
+                                
+            if args.recursive:
+                try:
+                    dest_group_users = iam_dest.get_group(group_name)
+                    dest_group_users_list = get_group_users(dest_group_users['get_group_response']['get_group_result']['users'] )
+                except:
+                    dest_group_users_list = []
+                src_group_user_list = get_group_users(src_group_users['get_group_response']['get_group_result']['users'] )           
+                dest_users_dict = add_users(iam_dest, dest_users_dict, args, src_group_user_list, dest_group_users_list, True)
+
                  
     
     if args.users:
-        add_users(iam_dst, args, args.users, False)
+        add_users(iam_dest, args, args.users, False, False)
 
 
 
